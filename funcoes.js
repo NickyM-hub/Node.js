f5d5866fadde2dc61c9ed643f6f616f24f31a3f4
Nicole Moura <moura2007nick@gmail.com>
2026-01-30 11:17:47 -0300

    arrow function

--- funcoes.js
+++ funcoes.js
@@ -1,13 +1,21 @@
-function saudacao(nome) {  // nome é um parâmetro da função
-    console.log('Olá', nome)
-}
+// function saudacao(nome) { // nome é um parametro da função saudacao
+//     console.log('Olá,', nome)
+// }
+
+const saudacao = nome => console.log('Olá', nome)
 
 saudacao('Nicole')  // 'Nicole' é um argumento passado para a função
 saudacao('Claudia')
 
-function calcularDobro(numero) {
-    return numero * 2
-}
+// function calcularDobro(numero) {
+//     return numero * 2
+// }
+
+//const calcularDobro = (numero) => {
+//    return numero * 2
+//}
+
+const calcularDobro = numero => numero * 2
 
 const numeroDobrado = calcularDobro(4)
 
